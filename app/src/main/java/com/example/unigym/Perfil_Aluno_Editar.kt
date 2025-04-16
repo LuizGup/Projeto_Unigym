9b47a9a52f6e5bd12094301de52bd22f18956b38
NicolasDev-web <nicolassantos2132005@gmail.com>
2025-04-16 17:14:18 -0300

    Está sendo feito o Perfil_Aluno_Editar.

---
+++ app/src/main/java/com/example/unigym/Perfil_Aluno_Editar.kt
@@ -0,0 +1,15 @@
+package com.example.unigym
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Perfil_Aluno_Editar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil_aluno_editar)
+
+        }
+    }
