b6954d53cac3ecd47310341ce2360393bda0c1f4
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2025-04-20 19:57:52 -0300

    Inicializando Tela Calendario de aulas

--- app/src/main/java/com/example/unigym/TelaCadastro_Aluno.kt
+++ app/src/main/java/com/example/unigym/TelaCadastro_Aluno.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Tela_CadastroAluno : AppCompatActivity() {
+class TelaCadastro_Aluno : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
