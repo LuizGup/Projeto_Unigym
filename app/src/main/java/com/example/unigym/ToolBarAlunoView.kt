f0fe6f5e9fde82e4bd591a127f75a35c1e123bed
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2025-04-22 13:35:43 -0300

    inicializar componente

---
+++ app/src/main/java/com/example/unigym/ToolBarAlunoView.kt
@@ -0,0 +1,49 @@
+package com.example.unigym
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.fragment.app.FragmentActivity
+import com.example.unigym.professor.Homepage_Professor
+import com.example.unigym.professor.CriarOpcaoDialog
+
+
+@SuppressLint("CustomViewStyleable")
+class ToolBarAlunoView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : LinearLayout(context, attrs) {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.activity_tool_bar_aluno_view, this, true)
+
+        // Lê o atributo personalizado
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ToolBarView)
+        val currentScreenName = typedArray.getString(R.styleable.ToolBarView_currentScreen)
+        typedArray.recycle()
+
+        val btnHome = findViewById<ImageButton>(R.id.btnHome)
+        val btnGym = findViewById<ImageButton>(R.id.btnGym)
+        val btnProfile = findViewById<ImageButton>(R.id.btnProfile)
+
+        btnHome.setOnClickListener {
+            if (currentScreenName != "Homepage_Professor") {
+                val intent = Intent(context, Homepage_Professor::class.java)
+                context.startActivity(intent)
+            }
+        }
+
+
+        btnGym.setOnClickListener {
+            // colocar logica de treino
+        }
+
+        btnProfile.setOnClickListener {
+            // colocar logica para entrar na tela do profile
+        }
+    }
+}
