b9264fff5121a7d49eff30644e837bd3f6c2eaff
Nelson Neto <111544659+Nelsolaa@users.noreply.github.com>
2025-04-21 23:08:51 -0300

    to cansado de explicar

---
+++ app/src/main/java/com/example/unigym/DialogEditarPerfilProfessor.kt
@@ -0,0 +1,32 @@
+package com.example.unigym
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import android.view.LayoutInflater
+import android.widget.ImageButton
+
+class DialogEditarPerfilProfessor : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val view = LayoutInflater.from(requireContext()).inflate(R.layout.activity_dialog_perfil_professor_confirmar_editar, null)
+
+        val btnConfirmarFicha = view.findViewById<ImageButton>(R.id.btnConfirmarFicha)
+        val btnCancelar = view.findViewById<ImageButton>(R.id.btnCancelar) // novo botão
+
+        btnConfirmarFicha.setOnClickListener {
+            // Ação ao clicar em "confirmar alterações"
+            dismiss()
+        }
+
+        btnCancelar.setOnClickListener {
+            // Apenas fecha o dialog
+            dismiss()
+        }
+
+        return AlertDialog.Builder(requireContext())
+            .setView(view)
+            .create()
+    }
+}
