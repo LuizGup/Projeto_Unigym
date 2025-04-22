8bf17c82b700cc3a03ef73874fedf75638b6a7d8
marcelo-filho0502 <marcelomrfilho@gmail.com>
2025-04-22 18:41:59 -0300

    Troca_de_telas_completas

--- app/src/main/java/com/example/unigym/Tela_Login.kt
+++ app/src/main/java/com/example/unigym/Tela_Login.kt
@@ -1,18 +1,32 @@
 package com.example.unigym
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.unigym.professor.Homepage_Professor
 
 class Tela_Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //Botoes de referencia
+        val esquecersenha= findViewById<Button>(R.id.esqueceusenha)
+        val entrar = findViewById<Button>(R.id.Entrar)
+        val ntemcadasttro = findViewById<Button>(R.id.Ntemcadastro)
+
+        entrar.setOnClickListener{
+            startActivity(Intent(this, Homepage_Professor::class.java))
+        }
+
+        esquecersenha.setOnClickListener{
+            startActivity(Intent(this,Tela_EsquecerSenha::class.java))
+        }
+        ntemcadasttro.setOnClickListener{
+            startActivity(Intent(this,TelaInicial::class.java))
         }
     }
 }
--- app/src/main/java/com/example/unigym/professor/Homepage_Professor.kt
+++ app/src/main/java/com/example/unigym/professor/Homepage_Professor.kt
@@ -8,7 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.unigym.R
 
-class Homepage_Professor : AppCompatActivity() {
+class    Homepage_Professor : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
