feeb8130115eda96ae75df74e4f8e765b8482457
marcelo-filho0502 <marcelomrfilho@gmail.com>
2025-04-23 17:27:42 -0300

    atualizou

--- app/src/main/java/com/example/unigym/aluno/TelaCadastro_Aluno.kt
+++ app/src/main/java/com/example/unigym/aluno/TelaCadastro_Aluno.kt
@@ -1,21 +1,25 @@
 package com.example.unigym.aluno
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.unigym.R
+import com.example.unigym.professor.Homepage_Professor
 
 class TelaCadastro_Aluno : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_cadastro_aluno)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val entrar = findViewById<Button>(R.id.Criar_conta)
+
+        entrar.setOnClickListener{
+            startActivity(Intent(this, Homepage_Professor::class.java))
         }
     }
 }
