d5b37d85ef7cadeec8fc1a84b623a0b5f26fd773
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2025-04-23 23:33:48 -0300

    Init

--- app/src/main/java/com/example/unigym/professor/CriarTreino_Professor.kt
+++ app/src/main/java/com/example/unigym/professor/CriarTreino_Professor.kt
@@ -1,7 +1,9 @@
 package com.example.unigym.professor
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,6 +24,11 @@ class CriarTreino_Professor : AppCompatActivity() {
         btnConfirmarFicha.setOnClickListener {
             val dialog = DialogMarcarAula_Professor()
             dialog.show(supportFragmentManager, "DialogMarcarAula_Professor")
+        }
+        val btnAdicionarExercicio = findViewById<ImageButton>(R.id.btnAdicionarExercicio)
+        btnAdicionarExercicio.setOnClickListener {
+            val intent = Intent(this, CriarTreino_Professor::class.java)
+            startActivity(intent)
         }
     }
 }
