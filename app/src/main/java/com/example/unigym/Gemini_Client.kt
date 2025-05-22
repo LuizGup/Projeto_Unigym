bed72c84ef10da74162fa8765146c5c601869cd0
NicolasDev-web <nicolassantos2132005@gmail.com>
2025-05-21 22:53:56 -0300

    começando o gemini

--- app/src/main/java/com/example/unigym/Gemini_Client.kt
+++ app/src/main/java/com/example/unigym/Gemini_Client.kt
@@ -5,7 +5,7 @@ import com.google.ai.client.generativeai.GenerativeModel
 class GeminiClient {
     companion object {
         private const val API_KEY = "AIzaSyBylaNNt99ps5NlJmWL13W9-Ekqk8lUp1I"
-    }
+
 
     val generativeModel: GenerativeModel by lazy {
         GenerativeModel(
@@ -13,4 +13,4 @@ class GeminiClient {
             apiKey = (API_KEY)
         )
     }
-}
+} }
