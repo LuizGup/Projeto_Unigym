49d23fd74b0b36b98ac999385bb5e9defeaa66d8
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2025-05-27 08:59:17 -0300

    BuildGradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.constraintlayout)
-
+    
+    implementation(libs.generativeai)
+    implementation(libs.google.generativeai)
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.androidx.annotation)
     implementation(libs.androidx.lifecycle.livedata.ktx)
